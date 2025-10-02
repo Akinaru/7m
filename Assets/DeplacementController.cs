4876233123c0b4ec65461532c65d01bbc94975d2
Maxime Gallotta <maxime@genesii.fr>
2025-10-02 15:00:37 +0200

    Fix player movements + bus

---
+++ Assets/DeplacementController.cs
@@ -0,0 +1,41 @@
+// DeplacementController.cs
+using UnityEngine;
+
+public class DeplacementController : BaseController<DeplacementController>
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    Transform playerRoot;
+    Vector2 moveInput;
+
+    void Awake()
+    {
+        var pc = PlayerController.Instance;
+        playerRoot = pc && pc.playerRoot ? pc.playerRoot : transform;
+        rb = playerRoot.GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        if (InputController.Instance != null)
+            InputController.Instance.OnMove += OnMoveInput;
+    }
+
+    void OnDisable()
+    {
+        if (InputController.Instance != null)
+            InputController.Instance.OnMove -= OnMoveInput;
+    }
+
+    void OnMoveInput(Vector2 v) => moveInput = v;
+
+    void FixedUpdate()
+    {
+        Vector3 dir = playerRoot.right * moveInput.x + playerRoot.forward * moveInput.y;
+        if (dir.sqrMagnitude > 1f) dir.Normalize();
+
+        Vector3 hv = dir * moveSpeed;
+        rb.velocity = new Vector3(hv.x, rb.velocity.y, hv.z); // conserve la gravité
+    }
+}
--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -1,12 +1,11 @@
+// InputController.cs
 using System;
 using UnityEngine;
 
 public class InputController : BaseController<InputController>
 {
     public event Action<Vector2> OnMove; // x = droite/gauche, y = avant/arrière
-    public event Action<Vector2> OnLook; // x = yaw, y = pitch (delta souris)
-
-    const float MouseDeadzone = 0.01f;
+    public event Action<Vector2> OnLook; // x = yaw, y = pitch
 
     void Start()
     {
@@ -16,36 +15,16 @@ public class InputController : BaseController<InputController>
 
     void Update()
     {
-        int right = (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ? 1 : 0)
-                  - (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
-        int fwd = (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow) ? 1 : 0)
-                  - (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ? 1 : 0);
+        // Déplacement (ZQSD / flèches)
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 move = new Vector2(x, y);
+        if (move.sqrMagnitude > 1f) move.Normalize();
+        OnMove?.Invoke(move);
 
-        if (right == 0 && fwd == 0)
-        {
-            float axH = Input.GetAxisRaw("Horizontal");
-            float axV = Input.GetAxisRaw("Vertical");
-            if (Mathf.Abs(axH) > 0.01f || Mathf.Abs(axV) > 0.01f)
-            {
-                right = axH > 0.01f ? 1 : (axH < -0.01f ? -1 : 0);
-                fwd = axV > 0.01f ? 1 : (axV < -0.01f ? -1 : 0);
-            }
-        }
-
-        OnMove?.Invoke(new Vector2(right, fwd));
-
+        // Souris
         float mx = Input.GetAxis("Mouse X");
         float my = Input.GetAxis("Mouse Y");
-        if (Mathf.Abs(mx) < MouseDeadzone) mx = 0f;
-        if (Mathf.Abs(my) < MouseDeadzone) my = 0f;
-
-        if (mx != 0f || my != 0f)
-            OnLook?.Invoke(new Vector2(mx, my));
-    }
-
-    public void SetCursorLocked(bool locked)
-    {
-        Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
-        Cursor.visible = !locked;
+        if (mx != 0f || my != 0f) OnLook?.Invoke(new Vector2(mx, my));
     }
 }
